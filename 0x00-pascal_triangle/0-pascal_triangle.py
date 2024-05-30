d729ae6eda4e16bb4277f52fb0ad3c8e688d5be3
Tau <taumasaire@gmail.com>
2024-05-30 15:32:17 +0200

    Impementing Pascal's triangle in Python

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,26 @@
+def pascal_triangle(n):
+    """
+    Generates Pascal's triangle up to the given number of rows.
+
+    Args:
+        n (int): The number of rows to generate in Pascal's triangle.
+
+    Returns:
+        list: A list of lists representing Pascal's triangle.
+
+    Example:
+        >>> pascal_triangle(5)
+        [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]]
+    """
+    triangle = []
+
+    if n <= 0:
+        return []
+
+    for i in range(n):
+        row = [1] * (i + 1)
+        for j in range(1, i):
+            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+        triangle.append(row)
+
+    return triangle
---
+++ 0x00-pascal_triangle/README.md
@@ -0,0 +1,3 @@
+# Pascal Triangle
+
+This project implements the Pascal Triangle in Python. The Pascal Triangle is a triangular array of binomial coefficients, named after the French mathematician Blaise Pascal. Each number in the triangle is the sum of the two numbers directly above it.

39217d49036e02f35e630e9d45a3804174fea103
Tau <taumasaire@gmail.com>
2024-06-13 12:17:21 +0200

    Adding minoperations script

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""This module contains the minOperations function"""
+
+
+def minOperations(n):
+    """
+    Returns the minimum number of operations
+    needed to result in exactly n H characters in the file.
+    """
+    if n <= 1:
+        return 0
+
+    operations = 0
+    divisor = 2
+
+    while n > 1:
+        while n % divisor == 0:
+            operations += divisor
+            n = n / divisor
+        divisor += 1
+
+    return operations
---
+++ 0x02-minimum_operations/README.md
@@ -0,0 +1,3 @@
+# README
+
+This directory contains the implementation of a function that calculates the minimum number of operations needed to transform a single 'H' in a text file into exactly n 'H' characters using only two operations: "Copy All" and "Paste".

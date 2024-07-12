793733a044b6d073ddc0400da3f62804b60a06a4
Tau <taumasaire@gmail.com>
2024-07-12 21:12:39 +0200

    updating characters display format

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,32 +2,43 @@
 
 const request = require('request');
 
+const getCharacterName = (url) => {
+  return new Promise((resolve, reject) => {
+    request(url, { json: true }, (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        reject(new Error(`Failed to retrieve character data from ${url}`));
+      } else {
+        resolve(body.name);
+      }
+    });
+  });
+};
+
 const getMovieCharacters = (movieId) => {
   const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
-  request(url, { json: true }, (error, response, body) => {
+  request(url, { json: true }, async (error, response, body) => {
     if (error || response.statusCode !== 200) {
-      console.error(`Failed to retrieve data for movie ID ${movieId}`);
+      console.error(new Error(`Failed to retrieve data for movie ID ${movieId}`));
       return;
     }
 
     const charactersUrls = body.characters;
 
-    charactersUrls.forEach((characterUrl) => {
-      request(characterUrl, { json: true }, (characterError, characterResponse, characterBody) => {
-        if (characterError || characterResponse.statusCode !== 200) {
-          console.error(`Failed to retrieve character data from ${characterUrl}`);
-        } else {
-          console.log(characterBody.name);
-        }
-      });
-    });
+    for (const characterUrl of charactersUrls) {
+      try {
+        const characterName = await getCharacterName(characterUrl);
+        console.log(characterName);
+      } catch (error) {
+        console.error(error);
+      }
+    }
   });
 };
 
 const movieId = process.argv[2];
 if (!movieId) {
-  console.error('Usage: ./0-starwars_characters.js <movie_id>');
+  console.error(new Error('Usage: ./0-starwars_characters.js <movie_id>'));
   process.exit(1);
 }
 

cd638e55acd4858f920f711f734d2a62d973e1db
Tau <taumasaire@gmail.com>
2024-08-07 20:04:07 +0200

    updating linting

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -12,12 +12,10 @@ def sieve_of_eratosthenes(n):
     """
     is_prime = [True] * (n + 1)
     is_prime[0] = is_prime[1] = False
-    p = 2
-    while (p * p <= n):
+    for p in range(2, int(n**0.5) + 1):
         if is_prime[p]:
-            for i in range(p * p, n + 1, p):
-                is_prime[i] = False
-        p += 1
+            for multiple in range(p * p, n + 1, p):
+                is_prime[multiple] = False
     return [p for p in range(n + 1) if is_prime[p]]
 
 
